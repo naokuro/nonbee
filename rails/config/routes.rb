f73c3cd49863ab3d2f1cce5a4599b571e481a127
naokuro <balance22@gmail.com>
2018-11-12 18:46:48 +0900

    apiのルーティング設定

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   get 'top/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  # api
+  namespace :api, { format: 'json' } do
+    namespace :v1 do
+
+    end
+  end
+
 end
